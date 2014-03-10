c3a94c1d859d2bb26fa69405681daef38ae5d9eb
varunreddy <varunreddy.nalagatla@davita.com>
2014-03-09 22:55:57 -0600

    adding but not yet supported build and packaging

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+// Generated on 2013-09-23 using generator-angular 0.4.0
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
